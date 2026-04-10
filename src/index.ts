8f5469c5c7a3d0a928821b28dcd9269bf3bf1aac
TheDreWen <louis.charneux@gmail.com>
2026-04-10 14:25:36 +0200

    Update command file extension filter from .js to .ts in index.ts

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
